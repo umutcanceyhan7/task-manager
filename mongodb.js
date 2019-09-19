ac3b593641d46509dc562f8d7257d83c74109d14
Umutcan <umutcanceyhan@gmail.com>
2019-09-19 12:31:46 +0300

    database changes

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,6 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-
+const {MongoClient, ObjectID} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'  
 
@@ -23,7 +21,7 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
 //         console.log(result.ops)
 //         // console.log(result.insertedCount)
 //         // console.log(result.ops)
-//         // console.log(result.insertedId)
+//         // console.log(result.insertedId)g
 //         // console.log(result.result)
 //         // console.log(result.connection)
 //     })
@@ -46,26 +44,51 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
 //     console.log(result.ops)
 // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Do homework',
-            completed: false
-        },
-        {
-            description: 'Throw out the trash',
-            completed: false
-        },
-        {
-            description: 'Listen CEZA',
-            completed: true
-        }
-    ], (error, result) => {
-        if(error) {
-            return console.log('Unable to add tasks')
-        }
-        console.log(result.ops)
-        console.log(result.result)
+//     db.collection('tasks').insertMany([
+//         {
+//             description: 'Do homework',
+//             completed: false
+//         },
+//         {
+//             description: 'Throw out the trash',
+//             completed: false
+//         },
+//         {
+//             description: 'Listen CEZA',
+//             completed: true
+//         }
+//     ], (error, result) => {
+//         if(error) {
+//             return console.log('Unable to add tasks')
+//         }
+//         console.log(result.ops)
+//         console.log(result.result)
 
+//     })
+
+
+    // db.collection('users').findOne({_id: new ObjectID('5d82260f48434709a838b76d')}, (error, user) => {
+    //     if(error){
+    //       return  console.log('Unable to fetch user')
+    //     }       
+    //     console.log(user)  
+    // })
+
+    // db.collection('users').find({ name:'Umutcan' }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    db.collection('tasks').findOne({_id: new ObjectID('5d822beca493411e18ec4594')}, (error, task) => {
+        if(error){
+            return console.log('Unable to find the task')
+        }
+        // console.log(task)
     })
 
- })
+    db.collection('tasks').find({completed: false}).toArray((error,tasks) => {
+        console.log(tasks)
+    })
+
+  })
+
+    
